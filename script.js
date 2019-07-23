b8d759f61cb6a41f268ea4f071898de8281e68da
admapop <52109936+admapop@users.noreply.github.com>
2019-07-23 13:54:38 +0200

    Multi Invoice + extra columns. Rudimentary system to cope with variants
    Can now drop all invoices in folder and run. It will save a CSV file with all the information.
    Works for main suppliers, but very finicky with random ones.

--- script.js
+++ script.js
@@ -2,34 +2,85 @@ const convert = require('xml-js');
 const { parse } = require('json2csv');
 const fs = require('fs');
 
-const fatturaXML = fs.readFileSync('./xml_test.xml');
+let fileArray = fs.readdirSync('./xmls/');
 
-const json = convert.xml2json(fatturaXML, {compact: true, spaces: 4});
-// fs.writeFileSync('fattura.json', json, err => {if(err) console.log(err)});
-
-let fattura = JSON.parse(json);
-let dataScadenza = fattura['p:FatturaElettronica'].FatturaElettronicaBody.DatiPagamento.DettaglioPagamento.DataScadenzaPagamento['_text'];
-let numeroFattura = fattura['p:FatturaElettronica'].FatturaElettronicaBody.DatiGenerali.DatiGeneraliDocumento.Numero['_text'];
-let fornitore = fattura['p:FatturaElettronica'].FatturaElettronicaHeader.CedentePrestatore.DatiAnagrafici.Anagrafica.Denominazione['_text'];
-
-let myData = {
-    'Numero Fattura': numeroFattura,
-    'Fornitore': fornitore,
-    'Data Scadenza': dataScadenza
+let myData = [];
+let individualFattura = {
+    'Numero Fattura': '',
+    'Fornitore': '',
+    'Data Fattura': '',
+    'Data Scadenza': '',
+    'Importo': ''
 }
 
-// console.log(numeroFattura, dataScadenza)
+try {
+    fileArray.forEach((file) => {
+        const fatturaXML = fs.readFileSync('./xmls/' + file);
+        const json = convert.xml2json(fatturaXML, { compact: true, spaces: 4 });
+        let fattura = JSON.parse(json);
+        let numeroFattura;
+        let dataScadenza;
+        let dataFattura;
+        let fornitore;
+        let importo;
+        if (fattura['p:FatturaElettronica'] !== undefined) { //standard
+            dataScadenza = fattura['p:FatturaElettronica'].FatturaElettronicaBody.DatiPagamento.DettaglioPagamento.DataScadenzaPagamento['_text'];
+            numeroFattura = fattura['p:FatturaElettronica'].FatturaElettronicaBody.DatiGenerali.DatiGeneraliDocumento.Numero['_text'];
+            dataFattura = fattura['p:FatturaElettronica'].FatturaElettronicaBody.DatiGenerali.DatiGeneraliDocumento.Data['_text'];
+            fornitore = fattura['p:FatturaElettronica'].FatturaElettronicaHeader.CedentePrestatore.DatiAnagrafici.Anagrafica.Denominazione['_text'];
+            importo = fattura['p:FatturaElettronica'].FatturaElettronicaBody.DatiPagamento.DettaglioPagamento.ImportoPagamento['_text'];
+        } else if(fattura['b:FatturaElettronica'] !== undefined) { //Fine Food
+            dataScadenza = fattura['b:FatturaElettronica'].FatturaElettronicaBody.DatiPagamento.DettaglioPagamento.DataScadenzaPagamento['_text'];
+            numeroFattura = fattura['b:FatturaElettronica'].FatturaElettronicaBody.DatiGenerali.DatiGeneraliDocumento.Numero['_text'];
+            dataFattura = fattura['b:FatturaElettronica'].FatturaElettronicaBody.DatiGenerali.DatiGeneraliDocumento.Data['_text'];
+            fornitore = fattura['b:FatturaElettronica'].FatturaElettronicaHeader.CedentePrestatore.DatiAnagrafici.Anagrafica.Denominazione['_text'];
+            importo = fattura['b:FatturaElettronica'].FatturaElettronicaBody.DatiPagamento.DettaglioPagamento.ImportoPagamento['_text'];
+        } else if(fattura.FatturaElettronica !== undefined) { //Carlsberg
+            dataScadenza = fattura.FatturaElettronica.FatturaElettronicaBody.DatiPagamento.DettaglioPagamento.DataScadenzaPagamento['_text'];
+            numeroFattura = fattura.FatturaElettronica.FatturaElettronicaBody.DatiGenerali.DatiGeneraliDocumento.Numero['_text'];
+            dataFattura = fattura.FatturaElettronica.FatturaElettronicaBody.DatiGenerali.DatiGeneraliDocumento.Data['_text'];
+            fornitore = fattura.FatturaElettronica.FatturaElettronicaHeader.CedentePrestatore.DatiAnagrafici.Anagrafica.Denominazione['_text'];
+            importo = fattura.FatturaElettronica.FatturaElettronicaBody.DatiPagamento.DettaglioPagamento.ImportoPagamento['_text'];
+        } else if(fattura['n0:FatturaElettronica'] !== undefined) { //Coca Cola
+            dataScadenza = fattura['n0:FatturaElettronica'].FatturaElettronicaBody.DatiPagamento.DettaglioPagamento.DataScadenzaPagamento['_text'];
+            numeroFattura = fattura['n0:FatturaElettronica'].FatturaElettronicaBody.DatiGenerali.DatiGeneraliDocumento.Numero['_text'];
+            dataFattura = fattura['n0:FatturaElettronica'].FatturaElettronicaBody.DatiGenerali.DatiGeneraliDocumento.Data['_text'];
+            fornitore = fattura['n0:FatturaElettronica'].FatturaElettronicaHeader.CedentePrestatore.DatiAnagrafici.Anagrafica.Denominazione['_text'];
+            importo = fattura['n0:FatturaElettronica'].FatturaElettronicaBody.DatiPagamento.DettaglioPagamento.ImportoPagamento['_text'];
+        } else if(fattura['q1:FatturaElettronica'] !== undefined){ //random
+            dataScadenza = fattura['q1:FatturaElettronica'].FatturaElettronicaBody.DatiPagamento.DettaglioPagamento.DataScadenzaPagamento['_text'];
+            numeroFattura = fattura['q1:FatturaElettronica'].FatturaElettronicaBody.DatiGenerali.DatiGeneraliDocumento.Numero['_text'];
+            dataFattura = fattura['q1:FatturaElettronica'].FatturaElettronicaBody.DatiGenerali.DatiGeneraliDocumento.Data['_text'];
+            fornitore = fattura['q1:FatturaElettronica'].FatturaElettronicaHeader.CedentePrestatore.DatiAnagrafici.Anagrafica.Denominazione['_text'];
+            importo = fattura['q1:FatturaElettronica'].FatturaElettronicaBody.DatiPagamento.DettaglioPagamento.ImportoPagamento['_text'];
+        } else { //random
+            dataScadenza = fattura['ns5:FatturaElettronica'].FatturaElettronicaBody.DatiPagamento.DettaglioPagamento.DataScadenzaPagamento['_text'];
+            numeroFattura = fattura['ns5:FatturaElettronica'].FatturaElettronicaBody.DatiGenerali.DatiGeneraliDocumento.Numero['_text'];
+            dataFattura = fattura['ns5:FatturaElettronica'].FatturaElettronicaBody.DatiGenerali.DatiGeneraliDocumento.Data['_text'];
+            fornitore = fattura['ns5:FatturaElettronica'].FatturaElettronicaHeader.CedentePrestatore.DatiAnagrafici.Anagrafica.Denominazione['_text'];
+            importo = fattura['ns5:FatturaElettronica'].FatturaElettronicaBody.DatiPagamento.DettaglioPagamento.ImportoPagamento['_text'];
+        }
 
-// console.log(fattura['p:FatturaElettronica'].FatturaElettronicaBody.DatiPagamento.DettaglioPagamento.DataScadenzaPagamento)
+        individualFattura = {
+            'Numero Fattura': numeroFattura,
+            'Fornitore': fornitore,
+            'Data Fattura': dataFattura,
+            'Data Scadenza': dataScadenza,
+            'Importo': importo
+        }
+        myData.push(individualFattura);
+    })
+} catch (err) {
+    console.log(err)
+}
 
-
-const fields = ['Numero Fattura', 'Fornitore','Data Scadenza'];
-const opts = {fields};
+const fields = ['Numero Fattura', 'Fornitore', 'Data Fattura', 'Data Scadenza', 'Importo'];
+const opts = { fields };
 
 try {
     const csv = parse(myData, opts);
-    // fs.writeFileSync('Fatture.csv', csv, err => {if(err) console.log(err)})
+    fs.writeFileSync('Fatture.csv', csv, err => {if(err) console.log(err)})
     console.log(csv);
-  } catch (err) {
-    console.error(err);
-  }
+} catch (err) {
+    console.error(err.name);
+}
--- test.js
+++ test.js
@@ -1,5 +1,56 @@
+const convert = require('xml-js');
+const { parse } = require('json2csv');
 const fs = require('fs');
 
-const fattura = fs.readFileSync('./fattura.json');
-let fatturaJSON = JSON.parse(fattura);
-console.log(fatturaJSON.FatturaElettronica.FatturaElettronicaBody.DatiPagamento.DettaglioPagamento.DataScadenzaPagamento)
+let test = fs.readdirSync('./xmls/');
+// console.log(test)
+
+const fatturaXML = fs.readFileSync('./xmls/239_IT12363410155_00tmJ.xml');
+const json = convert.xml2json(fatturaXML, { compact: true, spaces: 4 });
+// fs.writeFileSync('fattura.json', json, err => {if(err) console.log(err)});
+let fattura = JSON.parse(json);
+console.log(fattura['n0:FatturaElettronica'])
+// let myData = [];
+// let individualFattura = {
+//     'Numero Fattura': '',
+//     'Fornitore': '',
+//     'Data Scadenza': ''
+// }
+
+// try {
+//     test.forEach((file) => {
+//         const fatturaXML = fs.readFileSync('./xmls/' + file);
+//         const json = convert.xml2json(fatturaXML, { compact: true, spaces: 4 });
+//         let fattura = JSON.parse(json);
+//         let dataScadenza = fattura['p:FatturaElettronica'].FatturaElettronicaBody.DatiPagamento.DettaglioPagamento.DataScadenzaPagamento['_text'];
+//         let numeroFattura = fattura['p:FatturaElettronica'].FatturaElettronicaBody.DatiGenerali.DatiGeneraliDocumento.Numero['_text'];
+//         let fornitore = fattura['p:FatturaElettronica'].FatturaElettronicaHeader.CedentePrestatore.DatiAnagrafici.Anagrafica.Denominazione['_text'];
+//         individualFattura = {
+//             'Numero Fattura': numeroFattura,
+//             'Fornitore': fornitore,
+//             'Data Scadenza': dataScadenza
+//         }
+//         myData.push(individualFattura);
+//     })
+//     console.log(myData)
+// } catch (err) {
+//     test.forEach((file) => {
+//         const fatturaXML = fs.readFileSync('./xmls/' + file);
+//         const json = convert.xml2json(fatturaXML, { compact: true, spaces: 4 });
+//         let fattura = JSON.parse(json);
+//         let dataScadenza = fattura.FatturaElettronicaBody.DatiPagamento.DettaglioPagamento.DataScadenzaPagamento['_text'];
+//         let numeroFattura = fattura.FatturaElettronicaBody.DatiGenerali.DatiGeneraliDocumento.Numero['_text'];
+//         let fornitore = fattura.FatturaElettronicaHeader.CedentePrestatore.DatiAnagrafici.Anagrafica.Denominazione['_text'];
+//         individualFattura = {
+//             'Numero Fattura': numeroFattura,
+//             'Fornitore': fornitore,
+//             'Data Scadenza': dataScadenza
+//         }
+//         myData.push(individualFattura);
+//     })
+//     console.log(err);
+// }
+
+
+
+// console.log(fattura['p:FatturaElettronica'].FatturaElettronicaBody.DatiPagamento.DettaglioPagamento.DataScadenzaPagamento['_text'])
